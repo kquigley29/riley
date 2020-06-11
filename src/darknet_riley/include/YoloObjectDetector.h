858a21acd0c9cdede34b205fb02fb8f09ca8525d
Keane Quigley <keaneq@protonmail.com>
2020-06-11 16:13:48 +0100

    update cmakelists

---
+++ src/darknet_riley/include/YoloObjectDetector.h
@@ -0,0 +1,25 @@
+// ---------------------------
+// Yolo Object Detector Header
+// ---------------------------
+
+
+// Includes
+#include <iostream>
+
+#include "../../darknet/include/darknet.h"
+
+
+#ifndef YOLO_OBJECT_DETECTOR_H
+#define YOLO_OBJECT_DETECTOR_H
+
+
+class YoloObjectDetector {
+
+public:
+    // Constructor and destructor
+    YoloObjectDetector();
+    virtual ~YoloObjectDetector();
+};
+
+
+#endif //YOLO_OBJECT_DETECTOR_H
---
+++ src/darknet_riley/src/YoloObjectDetector.cpp
@@ -0,0 +1,25 @@
+// ================================
+// Yolo Object Detector Source File
+// ================================
+
+
+// Includes
+#include "../include/YoloObjectDetector.h"
+
+
+// YoloObjectDetector Constructor
+YoloObjectDetector::YoloObjectDetector()
+{
+    std::cout << "I spy with my little eye..." << "\n"
+              << "something beginning with..." << "\n"
+              << "\n"
+              << "M        M" << "\n"
+              << "M M    M M" << "\n"
+              << "M   MM   M" << "\n"
+              << "M        M" << "\n"
+              << "M        M" << "\n";
+}
+
+
+// Default destructor
+YoloObjectDetector::~YoloObjectDetector() = default;
