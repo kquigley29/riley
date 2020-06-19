b4a2b57b4435e0766efc08f32730eb628fa53593
Keane Quigley <keaneq@protonmail.com>
2020-06-20 00:05:57 +0100

    begin on yolo

--- src/darknet_riley/include/YoloObjectDetector.h
+++ src/darknet_riley/include/YoloObjectDetector.h
@@ -7,12 +7,14 @@
 #include <string>
 #include <iostream>
 #include <opencv2/opencv.hpp>
-#include "darknet.h"
 
 
 extern "C" {
-#include "network.h"
+#include "darknet.h"
 }
+
+extern "C" image mat_to_image(cv::Mat);
+extern "C" cv::Mat image_to_mat(image);
 
 
 #ifndef RILEY_YOLOOBJECTDETECTOR_H
@@ -21,8 +23,33 @@ extern "C" {
 
 class YoloObjectDetector {
 public:
-    YoloObjectDetector();
-    ~YoloObjectDetector();
+    explicit YoloObjectDetector(char*, char*, char*); 
+    virtual ~YoloObjectDetector();
+
+    // Member functions
+    detection *detect(char*);
+
+private:
+    network *net;
+
+    image buff_[3];
+    image buff_letter_[3];
+    int buff_index_ = 0;
+    void *cap;
+    float FPS_ = 0;
+    const float THRESHOLD_ = 0.5;
+    const float HIER_ = 0.5;
+    int RUNNING_ = 0;
+
+    int DETECT_FRAME_ = 3;
+    int DETECT_INDEX_ = 0;
+    float **predictions;
+    float *avg;
+    int DETECT_DONE_ = 0;
+    int DETECT_TOTAL_ = 0;
+
+    int CLASSES = 3;
+    char** NAMES;
 };
 
 
--- src/darknet_riley/src/YoloObjectDetector.cpp
+++ src/darknet_riley/src/YoloObjectDetector.cpp
@@ -7,10 +7,20 @@
 #include "../include/YoloObjectDetector.h"
 
 
-YoloObjectDetector::YoloObjectDetector() {
+YoloObjectDetector::YoloObjectDetector(char *cfgfile, char *weightfile, char* video_source) {
 
     std::cout << "guys, my beehive died :(\n";
     std::cout << "            ~ pjreddie\n";
+
+    net = load_network(cfgfile, weightfile, 0);
+}
+
+
+detection *YoloObjectDetector::detect(char *video_src) {
+
+    image **alphabet = load_alphabet();
+    ;
+
 }
 
 
