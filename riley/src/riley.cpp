e2d54c656af52fd26ecd5c40d3385f9d3b48010c
keane <keaneq@protonmail.com>
2020-10-10 00:14:57 +0100

    simplify

--- riley/src/riley.cpp
+++ riley/src/riley.cpp
@@ -12,44 +12,36 @@ int main(int argc, char** argv) {
               << "Let's go!\n";
 
     if (argc == 5) {
-        if (strcmp(argv[1], "yolo") == 0) {
-            char *data_cfg = argv[2];
-            char *cfg = argv[3];
-            char *weights = argv[4];
+        char *data_cfg = argv[2];
+        char *cfg = argv[3];
+        char *weights = argv[4];
 
-            ObjectDetector detector(data_cfg, cfg, weights, 0);
+        ObjectDetector detector(data_cfg, cfg, weights, 0);
+
+        if (strcmp(argv[1], "yolo") == 0) {
             detector.detect();
             return 0;
         }
         if (strcmp(argv[1], "track") == 0) {
-            char *data_cfg = argv[2];
-            char *cfg = argv[3];
-            char *weights = argv[4];
 
-            ObjectDetector detector(data_cfg, cfg, weights, 0);
             detector.detect(true);
             return 0;
         }
     }
 
     if (argc == 6) {
-        if (strcmp(argv[1], "yolo") == 0) {
-            char *data_cfg = argv[2];
-            char *cfg = argv[3];
-            char *weights = argv[4];
-            char *video = argv[5];
+        char *data_cfg = argv[2];
+        char *cfg = argv[3];
+        char *weights = argv[4];
+        char *video = argv[5];
 
-            ObjectDetector detector(data_cfg, cfg, weights, video);
+        ObjectDetector detector(data_cfg, cfg, weights, video);
+
+        if (strcmp(argv[1], "yolo") == 0) {
             detector.detect();
             return 0;
         }
         if (strcmp(argv[1], "track") == 0) {
-            char *data_cfg = argv[2];
-            char *cfg = argv[3];
-            char *weights = argv[4];
-            char *video = argv[5];
-
-            ObjectDetector detector(data_cfg, cfg, weights, video);
             detector.detect(true);
             return 0;
         }
